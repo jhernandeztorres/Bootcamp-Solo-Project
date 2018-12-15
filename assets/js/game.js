24b2b4bda6491119373a3c010df61aa9f0284411
Jose Hernandez Torres <jose@hernandezt.com>
2018-12-14 18:51:52 -0700

    updated images. trying new html

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+// Create a new scene
+let gameScene = new Phaser.Scene('Game');
+
+// Configuration
+let config = {
+    type: Phaser.AUTO,
+    width: 5760,
+    height: 3240,
+    scene: gameScene
+}
